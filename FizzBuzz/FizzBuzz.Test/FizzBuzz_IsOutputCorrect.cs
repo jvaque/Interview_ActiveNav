c557db25f5207434b429c11ba789b03bb225670c
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2023-03-04 16:39:50 +0000

    Added more tests

--- FizzBuzz/FizzBuzz.Test/FizzBuzz_IsOutputCorrect.cs
+++ FizzBuzz/FizzBuzz.Test/FizzBuzz_IsOutputCorrect.cs
@@ -10,11 +10,33 @@ public class FizzBuzz_IsOutputCorrect
     {
     }
 
-    [Test]
-    public void Test_Fizz()
+    [TestCase(3)]
+    [TestCase(6)]
+    [TestCase(9)]
+    public void Test_ReturnFizz(int value)
     {
-        var result = FizzBuzz.Output(3);
+        var result = FizzBuzz.Output(value);
 
         Assert.That(result, Is.EqualTo("Fizz"));
+    }
+
+    [TestCase(5)]
+    [TestCase(10)]
+    [TestCase(20)]
+    public void Test_ReturnBuzz(int value)
+    {
+        var result = FizzBuzz.Output(value);
+
+        Assert.That(result, Is.EqualTo("Buzz"));
+    }
+
+    [TestCase(15)]
+    [TestCase(30)]
+    [TestCase(45)]
+    public void Test_ReturnFizzBuzz(int value)
+    {
+        var result = FizzBuzz.Output(value);
+
+        Assert.That(result, Is.EqualTo("FizzBuzz"));
     }
 }
